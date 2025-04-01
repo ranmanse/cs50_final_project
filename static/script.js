80f8ff1e55ad261e1a43ecb04d21002c44a585a4
Manse <manse@posteo.de>
2025-04-01 21:56:32 +0200

    Add infinite Scrolling and deactivate pagination

--- static/script.js
+++ static/script.js
@@ -1,48 +1,66 @@
-function showImage(feature) {
-    // Update image append code (Co-Pilot)
-    const imageContainer = document.getElementById('image-container');
-        
-    // 1) Create div element / subgrid per image
-    var d = document.createElement('div');
-    d.className += ("image_subgrid");
-    imageContainer.appendChild(d);
+/*
+#################################################
+### Main function to fill the image container ###
+### Sums up the following functions           ###
+#################################################
+*/
 
+function fillGallery(current_page){
+   
+    // 1 Get Bounding Box
+    bbox_coordinates = getBoundingBox();
 
-    // 2) Append title to subgrid
-    //https://stackoverflow.com/questions/4772774/how-do-i-create-a-link-using-javascript
-    var a = document.createElement('a');
-    a.className += ("link_text");
+    // 2 Intersect Flickr-GeoJSON with Bounding Box
+    var polygon_bbox = turf.bboxPolygon(bbox_coordinates);
+    var photosWithin = turf.pointsWithinPolygon(flickr_data, polygon_bbox);
+    //console.log(photosWithin);
 
-    var linkText = document.createTextNode(`${feature.properties.title} by ${feature.properties.ownername}`);
-    a.appendChild(linkText);
-    a.href = `https://www.flickr.com/photos/${feature.properties.owner}/${feature.properties.id}`, 'target =_blank';
-
-    // In neuem Tab öffnen: https://www.dhiwise.com/post/html-open-link-in-new-tab-improve-user-navigation
-    a.setAttribute('target', '_blank');
-    a.setAttribute('rel', 'noopener noreferrer');
-
-    d.appendChild(a);
+    // 3) Prepare pagination in order to display 30 images per page			     
+    let pages = calculatePages(photosWithin, current_page);
+               
+    let page_start = pages.page_start;
+    page_end = pages.page_end;
+    let array_start = pages.array_start;
+    let array_end = pages.array_end;
+            
+    // 4) Slice Flickr-Data according the page
+    var features_slice = photosWithin.features.slice(array_start, array_end)
     
-    // 3) Append image to subgrid	
-    const myImage = new Image();
-    myImage.src = `https://live.staticflickr.com/${feature.properties.server}/${feature.properties.id}_${feature.properties.secret}_c.jpg`;
-    myImage.className += ("image");
-    d.appendChild(myImage);
+    // 5) Loop through each feature of the slice and create an image element
+    features_slice.forEach(feature => {
+        showImage(feature);							
+    });
+        
+    // 6) Create div element / subgrid per button
+    //addPaginationButtons(page_start, page_end)
+
 }
 
+
+
+/*
+###############################
+### Get Bounding Box of Map ###
+###############################
+*/
 
 function getBoundingBox() {
     // Get bounds from map: https://runebook.dev/en/articles/leaflet/index/map-getbounds
-			var bounds = map.getBounds();
-			bound_miny = bounds.getSouthWest().lat;
-			bound_minx = bounds.getSouthWest().lng;
-			bound_maxy = bounds.getNorthEast().lat;
-			bound_maxx = bounds.getNorthEast().lng;
-            bbox = [bound_minx, bound_miny, bound_maxx, bound_maxy];
+    var bounds = map.getBounds();
+    bound_miny = bounds.getSouthWest().lat;
+    bound_minx = bounds.getSouthWest().lng;
+    bound_maxy = bounds.getNorthEast().lat;
+    bound_maxx = bounds.getNorthEast().lng;
+    bbox = [bound_minx, bound_miny, bound_maxx, bound_maxy];
 
-            return bbox;
+    return bbox;
 }
 
+/*
+##########################################################################################
+### Function calculates Pages in order to slice Flickr-GeoJSON into slices (30 photos) ###
+##########################################################################################
+*/
 
 function calculatePages(photosWithin, current_page) {
     var feature_length = photosWithin.features.length;
@@ -73,6 +91,67 @@ function calculatePages(photosWithin, current_page) {
 }
 
 
+/*
+########################################################################
+### Fuction loads image & title from Flickr-GeoJSON to image-container ###
+##########################################################################
+*/
+
+function showImage(feature) {
+    // Update image append code (Co-Pilot)
+    const imageContainer = document.getElementById('image-container');
+        
+    // 1) Create div element / subgrid per image
+    var d = document.createElement('div');
+    d.className += ("image_subgrid");
+    imageContainer.appendChild(d);
+
+
+    // 2) Append title to subgrid
+    //https://stackoverflow.com/questions/4772774/how-do-i-create-a-link-using-javascript
+    var a = document.createElement('a');
+    a.className += ("link_text");
+
+    var linkText = document.createTextNode(`${feature.properties.title} by ${feature.properties.ownername}`);
+    a.appendChild(linkText);
+    a.href = `https://www.flickr.com/photos/${feature.properties.owner}/${feature.properties.id}`, 'target =_blank';
+
+    //Open Link in new Tab: https://www.dhiwise.com/post/html-open-link-in-new-tab-improve-user-navigation
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+
+    d.appendChild(a);
+    
+    // 3) Append image to subgrid	
+    const myImage = new Image();
+    myImage.src = `https://live.staticflickr.com/${feature.properties.server}/${feature.properties.id}_${feature.properties.secret}_c.jpg`;
+    myImage.className += ("image");
+    d.appendChild(myImage);
+}
+
+
+
+
+/*
+###############################################################################################
+### Debounce function to limit the rate at which a function can fire for infinite scrolling ###
+###############################################################################################
+*/
+// https://javascript.plainenglish.io/effortless-infinite-scrolling-a-guide-to-dynamic-image-loading-3ff6e7a4a608
+
+function debounce(func, delay) {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
+
+
+// deptecated beacause of infinite scrolling:
+
+/*
 function addPaginationButtons(page_start, page_end) {
     const pagination = document.getElementById('pagination');
     pagination.innerHTML = ''; // Clear existing buttons
@@ -135,48 +214,5 @@ function addPaginationButtons(page_start, page_end) {
          fillGallery(current_page);
      }); 
     
-
-
 }
-
-
-
-
-
-
-function fillGallery(current_page){
-    // 1 Clear existing images/Container			
-			const imageContainer = document.getElementById('image-container');
-			imageContainer.innerHTML = ''; // Clear existing images
-			imageContainer.scrollTop;
-
-			// 2 Get Bounding Box
-			bbox_coordinates = getBoundingBox();
-
-			// 3 Intersect Flickr-GeoJSON with Bounding Box
-			var polygon_bbox = turf.bboxPolygon(bbox_coordinates);
-			var photosWithin = turf.pointsWithinPolygon(flickr_data, polygon_bbox);
-			//console.log(photosWithin);
-
-			// 4) Prepare pagination in order to display 30 images per page			     
-            let pages = calculatePages(photosWithin, current_page);
-			           
-			let page_start = pages.page_start;
-			let page_end = pages.page_end;
-			let array_start = pages.array_start;
-			let array_end = pages.array_end;
-					
-			// 5) Slice Flickr-Data according the page
-			var features_slice = photosWithin.features.slice(array_start, array_end)
-			
-			// 6) Loop through each feature of the slice and create an image element
-			features_slice.forEach(feature => {
-				showImage(feature);							
-			});
-		        
-			// 7) Create div element / subgrid per button
-			addPaginationButtons(page_start, page_end)
-			// Aktuelle Seite als Parameter übergeben
-
-            // 8) Add event listener to each button
-}
+     */
